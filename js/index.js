0921e5144110e00d0cbddd15c40ee45f8a183fac
PhungDucHai <haiphung15012002@gmail.com>
2022-08-23 13:14:33 +0700

    Final version

--- js/index.js
+++ js/index.js
@@ -24,19 +24,21 @@ searchInput.addEventListener('change', (e) => {
             cityName.innerHTML = data.name || DEFAULT_VALUE;
             lat = data.lat;
             lon = data.lon;
+
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`)
+                .then(async res => {
+                    const data = await res.json();
+                    console.log(data);
+        
+                    weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+                    weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                    temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        
+                    sunrise.innerHTML = moment.unix(data.sys.sunrise).format('LT') || DEFAULT_VALUE;
+                    sunset.innerHTML = moment.unix(data.sys.sunset).format('LT') || DEFAULT_VALUE;
+                    humidity.innerHTML = data.main.humidity  || DEFAULT_VALUE;
+                    windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+            });
         }); 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`)
-        .then(async res => {
-            const data = await res.json();
-            console.log(data);
 
-            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
-            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
-
-            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('LT') || DEFAULT_VALUE;
-            sunset.innerHTML = moment.unix(data.sys.sunset).format('LT') || DEFAULT_VALUE;
-            humidity.innerHTML = data.main.humidity  || DEFAULT_VALUE;
-            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
-        });
 });
